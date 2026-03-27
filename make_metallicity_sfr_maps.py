corrected flux units and improved dereddening

make_metallicity_sfr_maps.py
@@ -46,7 +46,7 @@ def compute_EB_V(Ha_flux, Hb_flux,verbose=False):
         
         #EB_V_limit_mask = unp.nominal_values(EB_V) < 5.0
         net_mask  = net_mask | new_mask | obs_ratio.mask #| EB_V_limit_mask
-        EB_V = unp.uarray(np.where(net_mask, unp.nominal_values(net_mask), np.nan), np.where(net_mask, unp.std_devs(net_mask), np.nan))
+        EB_V = unp.uarray(np.where(net_mask, np.nan, unp.nominal_values(EB_V)), np.where(net_mask, np.nan, unp.std_devs(EB_V)))
     
     else: # if it is scalar
         try:
@@ -73,6 +73,7 @@ def get_EB_V(fit_results, args):
 
     EB_V_map = compute_EB_V(Ha_map, Hb_map)
     EB_V_int = compute_EB_V(Ha_int, Hb_int)
+    #EB_V_map = np.zeros(np.shape(Ha_map))
 
     return EB_V_map, EB_V_int
 # --------------------------------------------------------------------------------------------------------------------
@@ -294,7 +295,7 @@ def get_Z_C19(fit_results, args):
     '''
     # ------getting appropriate emission lines and calibration coefficients--------------
     if args.Zdiag == 'R3':
-        line_map_arr, line_int_arr = get_emission_line_maps(fit_results, ['OIII-5007', 'H-beta'], args)
+        line_map_arr, line_int_arr = get_emission_line_maps(fit_results, ['OIII-5007', 'H-beta'], args, dered=False)
         if line_map_arr is None: return None, ufloat(np.nan, np.nan)
         
         ratio_map = take_safe_log_ratio(line_map_arr[0], line_map_arr[1])
@@ -655,8 +656,67 @@ def plot_quant_map(ax, quant, quant_maps, args, cmap='cividis', clabel='', takel
     '''
     quant_map, _, quant_int, quant_mask = get_quant_from_quant_maps(quant, quant_maps)
     quant_masked = np.ma.masked_where(quant_mask, quant_map)
+    if quant == 'logOH': quant +=  f' ({args.Zdiag})'
+    elif quant == 'sfr': quant =  quant.upper()
     ax = plot_2D_map(quant_masked, ax, f'{quant}: {quant_int:.2f}', args, cmap=cmap, clabel=clabel, takelog=takelog, vmin=vmin, vmax=vmax, hide_xaxis=hide_xaxis, hide_yaxis=hide_yaxis, hide_cbar=hide_cbar, hide_cbar_ticks=hide_cbar_ticks, cticks_integer=cticks_integer)
 
+    return ax
+
+def odr_fit(df_input, quant_x='distance_arcsec', quant_y='log_OH', recenter_y=False):
+    '''
+    Fits the x and y columns using WLS
+    Returns fitted parameters
+    '''
+    df = df_input[[quant_x, quant_y, quant_y + '_u']]
+    # ---------rescaling y-axis for better fit----------
+    if recenter_y and 'OH' in quant_y:
+        recenter_to = 8.0
+        df[quant_y] = df[quant_y] - recenter_to
+        print(f'\nDeb3541: in odr_fit(), recentering {quant_y} to {recenter_to}..')
+
+    df = df.dropna(axis=0)
+    try:
+        model = Model(lambda param, x: np.poly1d(param)(x))
+        data = RealData(df[quant_x], df[quant_y], sy=df[quant_y + '_u'] if quant_y + '_u' in df and np.sum(df[quant_y + '_u']) > 0 else None, sx=df[quant_x + '_u'] if quant_x + '_u' in df and np.sum(df[quant_x + '_u']) > 0 else None)
+        odr = ODR(data, model, beta0=[0., 0.])  # Initial guess for slope and intercept
+        output = odr.run()
+        linefit = [ufloat(output.beta[0], output.sd_beta[0]), ufloat(output.beta[1], output.sd_beta[1])]
+        if recenter_y and 'OH' in quant_y: linefit[1] = linefit[1] + recenter_to
+    except:
+        print(f'WARNING: Could not fit radial profile via ODR, returning nan fit parameters')
+        linefit = [ufloat(np.nan, np.nan), ufloat(np.nan, np.nan)]
+
+    return linefit
+
+def plot_fitted_line(ax, linefit, xarr, fit_color, args, quant='log_OH', label=None):
+    '''
+    Computes and plots the fitted line given the linear fit parameters, on an axisting axis handle
+    Returns axis handle
+    '''
+    xarr = np.linspace(np.min(xarr), np.max(xarr), 10)
+    if quant in ['logOH', 'Z', 'log_OH']: main_text = r'$\nabla$Z$_r$'
+    else: main_text = r'$\nabla$'
+
+    if len(np.shape(linefit)) == 1:
+        yarr = np.poly1d(linefit)(xarr)
+        y_fitted = unp.nominal_values(yarr)
+        y_low = unp.nominal_values(yarr) - unp.std_devs(yarr)
+        y_up = unp.nominal_values(yarr) + unp.std_devs(yarr)
+        value_text = f'{linefit[0]: .2f}'    
+    else:
+        linefit_with_unc = [ufloat(linefit[0][index], np.mean((linefit[0][index] - linefit[1][index], linefit[2][index] - linefit[0][index]))) for index in range(np.shape(linefit)[1])]
+        yarr = np.poly1d(linefit_with_unc)(xarr)
+        y_fitted = unp.nominal_values(yarr)
+        y_low = unp.nominal_values(yarr) - unp.std_devs(yarr)
+        y_up = unp.nominal_values(yarr) + unp.std_devs(yarr)
+        value_text = r'%.2f$^{%.2f}_{%.2f}$' % (linefit[0][0], linefit[0][0] - linefit[1][0], linefit[2][0] - linefit[0][0])
+    
+    ax.plot(xarr, y_fitted, color=fit_color, lw=1, ls='dashed')
+    ax.fill_between(xarr, y_low, y_up, color=fit_color, lw=0, alpha=0.3)
+
+    if label is None: label = main_text + ' = ' + value_text
+    ax.text(0.1, 0.05, label, c=fit_color, fontsize=args.fontsize / args.fontfactor, ha='left', va='bottom', transform=ax.transAxes)
+    
     return ax
 
 def plot_met_sfr_corr(ax, quant_maps, args, color='salmon', colorby=None, cmap='cividis', hide_xaxis=False, hide_yaxis=False, clabel='', hide_cbar=True, hide_cbar_ticks=False, cticks_integer=True, log_sfr_min=None, log_sfr_max=None, logOH_min=None, logOH_max=None):
@@ -692,9 +752,7 @@ def plot_met_sfr_corr(ax, quant_maps, args, color='salmon', colorby=None, cmap='
     df = df.drop(columns=['logOH_mask', 'sfr_mask', 'sfr', 'sfr_u'])
     df = df.dropna(subset=['logOH', 'log_sfr']).reset_index(drop=True)
     
-    for col in ['log_sfr_u']: df = df[df[col] < np.percentile(df[col], 99)] # curtailing extreme outliers in uncertainty
-    
-
+    #for col in ['log_sfr_u']: df = df[df[col] < np.percentile(df[col], 99)] # curtailing extreme outliers in uncertainty
     print(f'{len(df)} of {ndf} spaxels remain with finite logOH and SFR')
 
     # -----------plotting scatter----------------------
@@ -703,6 +761,10 @@ def plot_met_sfr_corr(ax, quant_maps, args, color='salmon', colorby=None, cmap='
 
     p = ax.scatter(df['log_sfr'], df['logOH'], s=10, lw=1, ec='k', color=color)
     ax.errorbar(df['log_sfr'], df['logOH'], xerr=df['log_sfr_u'], yerr=df['logOH_u'], c=color, fmt='none', lw=0.5, alpha=0.7, zorder=-5)
+
+    # -----------fitting scatter plot----------------------
+    linefit_odr = odr_fit(df, quant_x='log_sfr', quant_y='logOH', recenter_y=True)
+    ax = plot_fitted_line(ax, linefit_odr, df['log_sfr'], 'salmon', args, quant='logOH', label=f'Slope = {linefit_odr[0]: .2f}')
 
     # -----------------annotating axes---------------------
     ax.set_xlim(log_sfr_min, log_sfr_max)
@@ -849,55 +911,55 @@ if __name__ == "__main__":
         args.maps_fits_file = maps_fits_dir / f'{args.id:05d}.maps.fits'
         args.quants_fits_file = quants_fits_dir / f'{args.id:05d}_Zdiag_{args.Zdiag}.quants.fits'
 
-        try:
-            # ---------measuring the various quantitites--------
-            if not os.path.exists(args.quants_fits_file) or args.clobber:
-                # -----------read in the emission line maps--------------
-                fit_results, spatial_header = read_line_maps_fits(args.maps_fits_file)
-                wcs = pywcs.WCS(spatial_header)
+        #try:
+        # ---------measuring the various quantitites--------
+        if not os.path.exists(args.quants_fits_file) or args.clobber:
+            # -----------read in the emission line maps--------------
+            fit_results, spatial_header = read_line_maps_fits(args.maps_fits_file)
+            wcs = pywcs.WCS(spatial_header)
 
-                args.available_lines = list(fit_results.keys())
-                if 'H-alpha' not in args.available_lines:
-                    print(f'ID {args.id} (z={args.z:.2f}) does not have H-alpha, so skipping it..')
-                    continue
-                if 'H-beta' not in args.available_lines:
-                    print(f'ID {args.id} (z={args.z:.2f}) does not have H-beta, so skipping it..')
-                    continue
+            args.available_lines = list(fit_results.keys())
+            if 'H-alpha' not in args.available_lines:
+                print(f'ID {args.id} (z={args.z:.2f}) does not have H-alpha, so skipping it..')
+                continue
+            if 'H-beta' not in args.available_lines:
+                print(f'ID {args.id} (z={args.z:.2f}) does not have H-beta, so skipping it..')
+                continue
 
-                args.EB_V_map, args.EB_V_int = get_EB_V(fit_results, args)
+            args.EB_V_map, args.EB_V_int = get_EB_V(fit_results, args)
 
-                # -----------computing various quantities--------------
-                quant_maps = compute_quant_maps(fit_results, args)
+            # -----------computing various quantities--------------
+            quant_maps = compute_quant_maps(fit_results, args)
 
-                # -----------save the quanttity maps in fits file-------------
-                save_quant_maps_fits(quant_maps, args.quants_fits_file, wcs, args)
-            else:
-                quant_maps, spatial_header = read_quant_maps_fits(args.quants_fits_file)
-            
-            # --------plot the qunatity maps-------------
-            if args.plot_metallicity:
-                fig, ax = plt.subplots(1, figsize=(8, 8), layout='constrained')
-                
-                ax = plot_quant_map(ax, 'logOH', quant_maps, args, cmap='cividis', takelog=False, vmin=logOH_min, vmax=logOH_max, hide_cbar=False)
-                
-                fig.text(0.1, 0.98, f'ID {args.id}', fontsize=args.fontsize, c='k', ha='left', va='top')
-                save_fig(fig, args.fig_dir, f'{args.id}_metallicity_{args.Zdiag}_maps.png', args)    
-
-            if args.plot_met_sfr:
-                fig, axes = plt.subplots(1, 3, figsize=(10, 4.5))
-                fig.subplots_adjust(left=0.06, right=0.98, bottom=0.12, top=0.9, wspace=0.5)
-                
-                axes[0] = plot_quant_map(axes[0], 'logOH', quant_maps, args, cmap='cividis', takelog=False, vmin=logOH_min, vmax=logOH_max, hide_cbar=False)
-                axes[1] = plot_quant_map(axes[1], 'sfr', quant_maps, args, cmap='Blues', takelog=True, vmin=log_sfr_min, vmax=log_sfr_max, hide_yaxis=True, hide_cbar=False)
-                axes[2] = plot_met_sfr_corr(axes[2], quant_maps, args, color='salmon', log_sfr_min=log_sfr_min, log_sfr_max=log_sfr_max, logOH_min=logOH_min, logOH_max=logOH_max)
-                
-                fig.text(0.05, 0.95, f'ID {args.id}', fontsize=args.fontsize, c='k', ha='left', va='top')
-                save_fig(fig, args.fig_dir, f'{args.id}_metallicity_{args.Zdiag}_SFR_corr.png', args)    
-
-            print(f'\nCompleted ID {args.id} in {timedelta(seconds=(datetime.now() - start_time2).seconds)}, {len(df) - index - 1} to go!')
+            # -----------save the quanttity maps in fits file-------------
+            save_quant_maps_fits(quant_maps, args.quants_fits_file, wcs, args)
+        else:
+            quant_maps, spatial_header = read_quant_maps_fits(args.quants_fits_file)
         
+        # --------plot the qunatity maps-------------
+        if args.plot_metallicity:
+            fig, ax = plt.subplots(1, figsize=(8, 8), layout='constrained')
+            
+            ax = plot_quant_map(ax, 'logOH', quant_maps, args, cmap='cividis', takelog=False, vmin=logOH_min, vmax=logOH_max, hide_cbar=False)
+            
+            fig.text(0.1, 0.98, f'ID {args.id}', fontsize=args.fontsize, c='k', ha='left', va='top')
+            save_fig(fig, args.fig_dir, f'{args.id}_metallicity_{args.Zdiag}_maps.png', args)    
+
+        if args.plot_met_sfr:
+            fig, axes = plt.subplots(1, 3, figsize=(10, 4.5))
+            fig.subplots_adjust(left=0.06, right=0.98, bottom=0.12, top=0.9, wspace=0.5)
+            
+            axes[0] = plot_quant_map(axes[0], 'logOH', quant_maps, args, cmap='cividis', takelog=False, vmin=logOH_min, vmax=logOH_max, hide_cbar=False)
+            axes[1] = plot_quant_map(axes[1], 'sfr', quant_maps, args, cmap='Blues', takelog=True, vmin=log_sfr_min, vmax=log_sfr_max, hide_yaxis=True, hide_cbar=False)
+            axes[2] = plot_met_sfr_corr(axes[2], quant_maps, args, color='salmon', log_sfr_min=log_sfr_min, log_sfr_max=log_sfr_max, logOH_min=logOH_min, logOH_max=logOH_max)
+            
+            fig.text(0.05, 0.95, f'ID {args.id}', fontsize=args.fontsize, c='k', ha='left', va='top')
+            save_fig(fig, args.fig_dir, f'{args.id}_metallicity_{args.Zdiag}_SFR_corr.png', args)    
+
+        print(f'\nCompleted ID {args.id} in {timedelta(seconds=(datetime.now() - start_time2).seconds)}, {len(df) - index - 1} to go!')
+        '''
         except Exception as e:
             print(f'Could not make plots for ID {args.id} because {e}')
             pass
- 
+        '''
     print(f'Completed in {timedelta(seconds=(datetime.now() - start_time).seconds)}')

make_msa3d_line_maps.py
@@ -4,10 +4,10 @@
     Author : Ayan
     Created: 06-02-26
     Example: run make_msa3d_line_maps.py --do_all_obj
-             run make_msa3d_line_maps.py --id 2145 --plot_flux_maps --save_linefit_plot --n_cores 4
-             run make_msa3d_line_maps.py --id 2145 --plot_line_maps
-             run make_msa3d_line_maps.py --id 2145 --debug_linefit 22,12
-             run make_msa3d_line_maps.py --do_all_obj --save_linefit_plot --plot_flux_maps --plot_line_maps --n_cores 6
+             run make_msa3d_line_maps.py --id 2145 --plot_line_flux_maps --save_linefit_plot --ncores 4
+             run make_msa3d_line_maps.py --id 2145 --plot_line_quant_maps
+             run make_msa3d_line_maps.py --id 2145 --debug_linefit 15,12
+             run make_msa3d_line_maps.py --do_all_obj --save_linefit_plot --plot_line_flux_maps --plot_line_quant_maps --ncores 6
 '''
 
 from header import *
@@ -60,10 +60,25 @@ def get_ifu_cube(cube_fits_file):
     wavelengths = wcs.all_pix2world(pixel_coords, 0)[:, 2]
     wavelengths *= 1e10 # from meters to Angstroms
 
-    return cube, cube_err, wavelengths, wcs
+    # -----------converting flux units from MJy/sr to ergs/s/cm^2/A-----------
+    w = pywcs.WCS(header)
+    pixel_area_deg2 = pywcs.utils.proj_plane_pixel_area(w) # pixel area in deg^2 directly from the CD matrix
+    pixel_area_sr = pixel_area_deg2 * (np.pi / 180)**2 # convert to sr
+    cube = cube * 1e6 * pixel_area_sr # conversion factor from MJy/sr to Jy/pix
+    cube_err = cube_err * 1e6 * pixel_area_sr # conversion factor from MJy/sr to Jy/pix
+    
+    factors = 2.99792458e-5 / (wavelengths**2) # conversion factor from Jy to ergs/s/cm^2/A
+    constant_factor = 1e-19
+
+    cube = cube * factors[:, np.newaxis, np.newaxis] / constant_factor
+    cube_err = cube_err * factors[:, np.newaxis, np.newaxis] / constant_factor
+
+
+
+    return cube, cube_err, wavelengths, wcs, constant_factor
 
 # --------------------------------------------------------------------------------------------------------------------
-def linefit_cube(cube, wavelengths, args, cube_err=None, flam_col='flam', flam_u_col='flam_u', wave_col='rest_wave', label_col='labels', cont_col='cont', snr_thresh=1.5):
+def linefit_cube(cube, wavelengths, args, cube_err=None, flam_col='flam', flam_u_col='flam_u', wave_col='rest_wave', label_col='labels', cont_col='cont', snr_thresh=1.5, flux_factor=1.):
     '''
     Runs spaxel-by-spaxel emission line fitting of the emission line list provided (as args.line_list)
     Accounts for the corresponding errorcube, if provided
@@ -99,13 +114,13 @@ def linefit_cube(cube, wavelengths, args, cube_err=None, flam_col='flam', flam_u
 
     start_time3 = datetime.now()
     # ----------parallelising--------------
-    if args.n_cores > 1:
+    if args.ncores > 1:
         spaxel_indices = [(x, y) for y in range(ny) for x in range(nx)]
-        worker_func = partial(linefit_spaxel, cube=cube, cube_err=cube_err, rest_wave_arr=rest_wave_arr, df_lines=df_lines, line_groups=line_groups, args=args, flam_col=flam_col, flam_u_col=flam_u_col, wave_col=wave_col, label_col=label_col, cont_col=cont_col, snr_thresh=snr_thresh) # Use partial to fix the arguments that don't change
+        worker_func = partial(linefit_spaxel, cube=cube, cube_err=cube_err, rest_wave_arr=rest_wave_arr, df_lines=df_lines, line_groups=line_groups, args=args, flam_col=flam_col, flam_u_col=flam_u_col, wave_col=wave_col, label_col=label_col, cont_col=cont_col, snr_thresh=snr_thresh, flux_factor=flux_factor) # Use partial to fix the arguments that don't change
 
-        print(f'\t\tStarting fit on {ny*nx} spaxels using {args.n_cores} cores...')
+        print(f'\t\tStarting fit on {ny*nx} spaxels using {args.ncores} cores...')
         
-        with Pool(processes=args.n_cores) as pool:
+        with Pool(processes=args.ncores) as pool:
             raw_results = pool.map(worker_func, spaxel_indices) # map returns results in the order of spaxel_indices
             
         for x, y, res in raw_results:
@@ -122,7 +137,7 @@ def linefit_cube(cube, wavelengths, args, cube_err=None, flam_col='flam', flam_u
                 if args.debug_linefit is not None: i, j = args.debug_linefit
 
                 # ------------calling line fitter for this spaxel---------------
-                i, j, fit_results_spaxel = linefit_spaxel((i, j), cube, cube_err, rest_wave_arr, df_lines, line_groups, args, flam_col=flam_col, flam_u_col=flam_u_col, wave_col=wave_col, label_col=label_col, cont_col=cont_col)                
+                i, j, fit_results_spaxel = linefit_spaxel((i, j), cube, cube_err, rest_wave_arr, df_lines, line_groups, args, flam_col=flam_col, flam_u_col=flam_u_col, wave_col=wave_col, label_col=label_col, cont_col=cont_col, flux_factor=flux_factor)                
                     
                 # ---------printing fit results and exiting, for debugging mode---------
                 if args.debug_linefit is not None:
@@ -148,7 +163,7 @@ def linefit_cube(cube, wavelengths, args, cube_err=None, flam_col='flam', flam_u
     return fit_results
 
 # --------------------------------------------------------------------------------------------------------------------
-def linefit_spaxel(coords, cube, cube_err, rest_wave_arr, df_lines, line_groups, args, flam_col='flam', flam_u_col='flam_u', wave_col='rest_wave', label_col='labels', cont_col='cont', snr_thresh=1.5):
+def linefit_spaxel(coords, cube, cube_err, rest_wave_arr, df_lines, line_groups, args, flam_col='flam', flam_u_col='flam_u', wave_col='rest_wave', label_col='labels', cont_col='cont', snr_thresh=1.5, flux_factor=1.):
     '''
     Runs emission line fitting on one spectrum (spaxel) of the emission line wavelengths provided (df_lines)
     Accounts for the corresponding error spectrum, if provided
@@ -188,7 +203,7 @@ def linefit_spaxel(coords, cube, cube_err, rest_wave_arr, df_lines, line_groups,
 
     # ------------calling line fitter for this spaxel---------------
     if len(df_spec) > 0:
-        fit_results_spaxel = linefit_spectrum(df_spec, df_lines, line_groups, args, i=i, j=j, flam_col=flam_col, flam_u_col=flam_u_col, wave_col=wave_col, label_col=label_col, cont_col=cont_col)                
+        fit_results_spaxel = linefit_spectrum(df_spec, df_lines, line_groups, args, i=i, j=j, flam_col=flam_col, flam_u_col=flam_u_col, wave_col=wave_col, label_col=label_col, cont_col=cont_col, flux_factor=flux_factor)                
     elif not args.silent:
         print(f'\t\t..no valid spectrum in this spaxel. Moving on..')
 
@@ -224,7 +239,7 @@ def global_gaussian_model(x, *popt, rest_waves=None, tie_vdisp=False):
     return model
 
 # --------------------------------------------------------------------------------------------------------------------
-def linefit_spectrum(df_spec, df_lines, line_groups, args, i='X', j='X', flam_col='flam', flam_u_col='flam_u', wave_col='rest_wave', label_col='labels', cont_col='cont'):
+def linefit_spectrum(df_spec, df_lines, line_groups, args, i='X', j='X', flam_col='flam', flam_u_col='flam_u', wave_col='rest_wave', label_col='labels', cont_col='cont', flux_factor=1.):
     '''
     Runs emission line fitting on one spectrum (spaxel) of the emission line wavelengths provided (df_lines)
     Accounts for the corresponding error spectrum, if provided
@@ -251,7 +266,7 @@ def linefit_spectrum(df_spec, df_lines, line_groups, args, i='X', j='X', flam_co
     if args.save_linefit_plot or args.debug_linefit is not None:
         fig, ax = plt.subplots(1, figsize=(12, 3), layout='constrained')
 
-        ax = plot_line_fit_spaxel(ax, df_spec, df_lines, args, popt=None, flam_col=flam_col, flam_u_col=flam_u_col, wave_col=wave_col, label_col=label_col, cont_col=cont_col, contsub_col=contsub_col)
+        ax = plot_line_fit_spaxel(ax, df_spec, df_lines, args, popt=None, flam_col=flam_col, flam_u_col=flam_u_col, wave_col=wave_col, label_col=label_col, cont_col=cont_col, contsub_col=contsub_col, flux_factor=flux_factor)
         ax = plot_linelist(ax, df_lines, fontsize=args.fontsize / args.fontfactor, color='cornflowerblue')
         for line_wave in df_lines[wave_col]: ax.axvspan(line_wave - args.mask_window, line_wave + args.mask_window, color='gray', alpha=0.2)
 
@@ -286,10 +301,9 @@ def linefit_spectrum(df_spec, df_lines, line_groups, args, i='X', j='X', flam_co
 
         try:
             popt, pcov = curve_fit(group_model, df_chunk[wave_col], df_chunk[contsub_col], p0=p0, sigma=df_chunk[flam_u_col], bounds=(lbounds, ubounds))
-
             perr = np.sqrt(np.diag(pcov))
             
-            # ------map[ing] parameters back to results------------
+            # ------mapping parameters back to results------------
             v_fit, v_err = popt[0], perr[0]
 
             for ind in range(len(g_labels)):
@@ -303,7 +317,7 @@ def linefit_spectrum(df_spec, df_lines, line_groups, args, i='X', j='X', flam_co
                 # ------Integrated Flux Calculation-0-----------
                 mu_w = g_waves[ind] * (1 + ufloat(v_fit, v_err) / c_km_s)
                 sig_w = (ufloat(sig_fit, sig_err) * mu_w) / c_km_s
-                flux = ufloat(amp_fit, amp_err) * sig_w * np.sqrt(2 * np.pi)
+                flux = ufloat(amp_fit, amp_err) * sig_w * np.sqrt(2 * np.pi) * flux_factor # multiplying the constant factor for flux here so that output fluxes are in units of ergs/s/cm^2
                 
                 fit_results_spaxel[g_labels[ind]] = {
                     'flux': flux.n, 'flux_err': flux.s,
@@ -312,12 +326,11 @@ def linefit_spectrum(df_spec, df_lines, line_groups, args, i='X', j='X', flam_co
                 }
             # ---------plotting the fit onto the observed spectrum-----------
             if args.save_linefit_plot or args.debug_linefit is not None:
-                norm_factor = 1.
                 x_arr = np.linspace(min_w, max_w, 100)
                 model_flux = group_model(x_arr, *popt)
                 model_cont = np.interp(x_arr, df_chunk[wave_col], df_chunk[cont_col])
                 total_model = model_flux + model_cont # need to add the continuum back in just for plotting purposes
-                ax.plot(x_arr, total_model / norm_factor, color='k', lw=1.5, linestyle='--', label='Fitted model', zorder=5)
+                ax.plot(x_arr, total_model, color='k', lw=1.5, linestyle='--', label='Fitted model', zorder=5)
 
         except Exception as e:
             print(f'Fit failed due to: {e}')
@@ -330,30 +343,28 @@ def linefit_spectrum(df_spec, df_lines, line_groups, args, i='X', j='X', flam_co
     return fit_results_spaxel
 
 # --------------------------------------------------------------------------------------------------------------------
-def plot_line_fit_spaxel(ax, df_spec, df_lines, args, popt=None, color='orangered', flam_col='flam', flam_u_col='flam_u', wave_col='rest_wave', label_col='labels', cont_col='cont', contsub_col='flam_contsub'):
+def plot_line_fit_spaxel(ax, df_spec, df_lines, args, popt=None, color='orangered', flam_col='flam', flam_u_col='flam_u', wave_col='rest_wave', label_col='labels', cont_col='cont', contsub_col='flam_contsub', flux_factor=1.):
     '''
     Plots the spectrum along one spaxel in the given axis handle, for debugging purposes
     Returns the axis handle
     '''
-    norm_factor = 1 #if args.show_log_flux else 1e-19
-
     # ---------------plot the observed spectrum---------------
-    ax.step(df_spec[wave_col], df_spec[flam_col] / norm_factor, lw=1, c=color, alpha=1, where='mid')
-    ax.plot(df_spec[wave_col], df_spec[cont_col] / norm_factor, lw=1, c='grey', alpha=1)
-    ax.fill_between(df_spec[wave_col], (df_spec[flam_col] - df_spec[flam_u_col]/2) / norm_factor, (df_spec[flam_col] + df_spec[flam_u_col]/2) / norm_factor, lw=0, color=color, alpha=0.5, step='pre')#, drawstyle='steps')
+    ax.step(df_spec[wave_col], df_spec[flam_col], lw=1, c=color, alpha=1, where='mid')
+    ax.plot(df_spec[wave_col], df_spec[cont_col], lw=1, c='grey', alpha=1)
+    ax.fill_between(df_spec[wave_col], (df_spec[flam_col] - df_spec[flam_u_col]/2), (df_spec[flam_col] + df_spec[flam_u_col]/2), lw=0, color=color, alpha=0.5, step='pre')#, drawstyle='steps')
 
     # -----------plot the fitted spectrum--------------------
     if popt is not None:
         x_arr = np.linspace(df_spec[wave_col].min(), df_spec[wave_col].max(), 1000)
         total_model = global_gaussian_model(x_arr, *popt, rest_waves=df_lines[wave_col].values, tie_vdisp=args.tie_vdisp)
-        ax.plot(x_arr, total_model / norm_factor, color='k', lw=1.5, linestyle='--', label='Fitted model', zorder=5)
+        ax.plot(x_arr, total_model, color='k', lw=1.5, linestyle='--', label='Fitted model', zorder=5)
 
     if args.show_log_flux: ax.set_yscale('log')
 
     # ----------------annotate axis labels---------------
     ax.set_xlabel(r'Rest-frame wavelength ($\AA$)', fontsize=args.fontsize)
-    #ylabel = r'f$_{\lambda}$ ' + '(%.0e ' % norm_factor + r'ergs/s/cm$^2$/A)'
-    ax.set_ylabel(r'f$_{\lambda}$ ergs/s/cm$^2$/A)', fontsize=args.fontsize)
+    if flux_factor == 1: ax.set_ylabel(r'f$_{\lambda}$ (ergs/s/cm$^2$/A)', fontsize=args.fontsize)
+    else: ax.set_ylabel(r'f$_{\lambda}$' + f'({flux_factor:.0e} ' + r'ergs/s/cm$^2$/A)', fontsize=args.fontsize)
     #if not args.show_log_flux: ax.set_ylim(0, args.flam_max) # flam_max should be in units of 1e-19 ergs/s/cm^2/A
     ax.tick_params(axis='both', which='major', labelsize=args.fontsize)
 
@@ -398,17 +409,12 @@ def get_emission_line_map(line, fit_results, args, log_flux_min=-1.5, log_flux_m
     line_map_err = fit_results[line]['flux_err']
     snr_map = line_map / line_map_err
 
-    # -------converting flux to flam units--------------
-    conversion_factor = 1.
-    line_map /= conversion_factor # now in ergs/s/cm^2/kpc^2
-    line_map_err /= conversion_factor # now in ergs/s/cm^2/kpc^2
-
     # --------curtailing to real values only-------------------
-    mask = ~(np.isfinite(line_map)) | (snr_map < 1e-3)
+    mask = ~(np.isfinite(line_map)) #| (snr_map < 1e-3)
     if log_flux_max is not None: mask = mask | (line_map > 10 ** log_flux_max)
     if log_flux_min is not None: mask = mask | (line_map < 10 ** log_flux_min)
-    line_map = np.where(mask, line_map, np.nan)
-    line_map_err = np.where(mask, line_map_err, np.nan)
+    line_map = np.where(mask, np.nan, line_map)
+    line_map_err = np.where(mask, np.nan, line_map_err)
 
     # ---------getting integrated flux------------
     ny, nx = np.shape(line_map)
@@ -526,7 +532,7 @@ def plot_line_flux_maps(fit_results, args):
         col = index % ncol
         
         # ------------extract linemap from fit_results dict--------------
-        line_map, _  = get_emission_line_map(fitted_line, fit_results, args, log_flux_min=None, log_flux_max=None, dered=True)
+        line_map, _  = get_emission_line_map(fitted_line, fit_results, args, log_flux_min=None, log_flux_max=None, dered=False)
         fluxmap = unp.nominal_values(line_map.data)
 
         # ------------plot linemap--------------
@@ -556,7 +562,7 @@ def plot_line_flux_maps(fit_results, args):
     return fig
 
 # --------------------------------------------------------------------------------------------------------------------
-def plot_line_maps(fit_results, line, args):
+def plot_line_quant_maps(fit_results, line, args):
     '''
     Plots the emission line flux and kinematic maps in one figure and saves it to file
     Returns the figure handle
@@ -680,10 +686,10 @@ if __name__ == "__main__":
         
         if not os.path.exists(args.maps_fits_file) or args.clobber or args.debug_linefit is not None:
             # -----------read in the cube--------------
-            cube, cube_err, wavelengths, wcs = get_ifu_cube(args.cube_fits_file)
+            cube, cube_err, wavelengths, wcs, flux_factor = get_ifu_cube(args.cube_fits_file)
 
             # -----------emission line fitting--------------
-            fit_results = linefit_cube(cube, wavelengths, args, cube_err=cube_err, snr_thresh=1.5)
+            fit_results = linefit_cube(cube, wavelengths, args, cube_err=cube_err, snr_thresh=1.5, flux_factor=flux_factor)
 
             # -----------save the emission maps in fits file-------------
             save_line_maps_fits(fit_results, args.maps_fits_file, wcs, args)
@@ -691,12 +697,12 @@ if __name__ == "__main__":
             fit_results, spatial_header = read_line_maps_fits(args.maps_fits_file)
         
         # --------plot the emission line maps-------------
-        if args.plot_flux_maps:
+        if args.plot_line_flux_maps:
             fig = plot_line_flux_maps(fit_results, args)
-        if args.plot_line_maps:
+        if args.plot_line_quant_maps:
             line = 'H-alpha'
             if line in list(fit_results.keys()):
-                fig = plot_line_maps(fit_results, line, args)
+                fig = plot_line_quant_maps(fit_results, line, args)
             else:
                 print(f'{line} is not available for object {args.id}')
 